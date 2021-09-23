50487b9d70bfe0e25e667f915fa11c439a610ecd
Jorel Martins <bearshamis@gmail.com>
2021-09-23 15:02:32 -0300

    Iniciando Tabela

--- src/App.js
+++ src/App.js
@@ -10,8 +10,33 @@ const App = () => {
         <input className='inputPadrao' type='password' required placeholder="Password:"></input>
         <button className="enviar" id='botao'>Enviar</button>
       </fieldset>
+
     </form>
-  </div>);
+
+    <table className="tabela">
+    <thead>
+    <tr>
+      <th>NOME</th>
+      <th>EMAIL</th>
+      <th>SENHA</th>
+      <th colSpan="2">AÇÕES</th>
+
+    </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Fulana da Silva</td>
+        <td>Fulana da Silva</td>
+        <td>Fulana da Silva</td>
+        <td>Fulana da Silva</td>
+      </tr>
+    </tbody>
+    </table>
+
+  </div>
+  
+  )
+  
 }
 
 
