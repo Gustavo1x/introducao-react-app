ad0347f4c091a90bd610f46b0561f1e6e10f5e86
janinelps <janinelpss@gmail.com>
2021-09-23 14:33:56 -0300

    Estilizando Formulario

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
         <legend>FORMULARIO DE CADASTRO</legend>
         <input className='inputPadrao' type='text' required placeholder="Nome:"></input>
         <input className='inputPadrao' type='email' required placeholder="Email:"></input>
-        <input className='inputPadrao' type='password' required placeholder="password:"></input>
+        <input className='inputPadrao' type='password' required placeholder="Password:"></input>
         <button className="enviar" id='botao'>Enviar</button>
       </fieldset>
     </form>
